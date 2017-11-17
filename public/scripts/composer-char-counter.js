42e81416101250c2a832ff87b0261b93ff256961
Karanveer-singh671 <singhkaranveer671@gmail.com>
2017-11-17 00:18:53 +0000

    editedout commented code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
   var maxLength = 140;
   $('.new-tweet textarea').keyup(function() {
-  var length = $(this).val().length;
-  var length = maxLength-length;
-  $('.counter').text(length);
-  if (length < 0){
-    $('.counter').css("color", "red");
-  }
-  else {
-     $('.counter').css("color", "black");
-  }
+    var length = $(this).val().length;
+    var length = maxLength - length;
+    $('.counter').text(length);
+    if (length < 0){
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "black");
+    }
 
+  });
 });
-})
 
 // need to close all brackets
 
